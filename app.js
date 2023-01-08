185ce261597242f2218bbc11fc08b824777f912d
sisko81 <74885506+sisko81@users.noreply.github.com>
2023-01-08 12:52:23 +0900

    Changed uppercase and lowercase function to just one function

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 //DOM elements.
 const resultEl = document.getElementById('result');
 const lengthEl = document.getElementById('length');
-const uppercaseEl = document.getElementById('uppercase');
-const lowercaseEl = document.getElementById('lowercase');
+const alphabetEl = document.getElementById('alphabet');
 const numbersEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
 const clipboardEl = document.getElementById('clipboard');
 
 const randomFunc = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
+    alphabet: getRandomAlphabet,
     number: getRandomNumber,
     symbol: getRandomSymbol
 };
@@ -18,14 +16,12 @@ const randomFunc = {
 //generate event listen
 generateEl.addEventListener('click', () => {
     const length = +lengthEl.value;
-    const hasLower = lowercaseEl.checked;
-    const hasUpper = uppercaseEl.checked;
+    const hasAlpha = alphabetEl.checked;
     const hasNumber = numbersEl.checked;
     const hasSymbols = symbolsEl.checked;
 
     resultEl.innerText = generatePassword(
-        hasLower,
-        hasUpper,
+        hasAlpha,
         hasNumber,
         hasSymbols,
         length  
@@ -51,7 +47,7 @@ clipboardEl.addEventListener('click', () => {
 })
 
 //Generate password function.
-function generatePassword(lower, upper, number, symbol, length ) {
+function generatePassword(alphabet, number, symbol, length ) {
     // 1. Init password variable.
     // 2. Filter out unchecked typed.
     // 3. Loop over length and call generator function for each type.
@@ -59,11 +55,11 @@ function generatePassword(lower, upper, number, symbol, length ) {
 
     let generatedPassword = '';
     
-    const typesCount = lower + upper + number + symbol;
+    const typesCount = alphabet + number + symbol;
 
     //console.log('typesCount: ', typesCount);
 
-    const typesArray = [{lower}, {upper}, {number}, {symbol}].filter
+    const typesArray = [{alphabet}, {number}, {symbol}].filter
     (
       item => Object.values(item)[0]
     );
@@ -90,20 +86,14 @@ function generatePassword(lower, upper, number, symbol, length ) {
 
 //Generator function.
 
-function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+function getRandomAlphabet() {
+    const alphabets = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return alphabets[Math.floor(Math.random()* alphabets.length)]
 }
-
-
-function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-}
-
 
 function getRandomNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
-
 
 function getRandomSymbol() {
     const symbols = '?!@#$%^&*()_ \|-{}[]<>/,.';
